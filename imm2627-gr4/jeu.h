f0def53d865e7c2fd4654f88ac1554a918f367d9
8fn4xc9f67-code <8fn4xc9f67@privaterelay.appleid.com>
2026-09-14 15:59:40 +0200

    Update jeu.h

--- imm2627-gr4/jeu.h
+++ imm2627-gr4/jeu.h
@@ -4,7 +4,7 @@
 #define ROWS 7
 #define COLS 11
 
-#define PION 0
+#define TOUR 0
 #define ROI  1
 
 #define BLEU  0
@@ -23,8 +23,15 @@ int jeu_nombre_pieces(void);
 Piece *jeu_piece(int index);
 int jeu_piece_at(int row, int col);
 int jeu_joueur_actuel(void);
+void jeu_changer_joueur(void);
 int jeu_peut_deplacer(int piece_index, int new_row, int new_col);
 int jeu_deplacer(int piece_index, int new_row, int new_col);
+int jeu_case_jaune(int row, int col);
 int jeu_valeur_case(int row, int col);
+int jeu_couleur_conquete(int row, int col);
+
+/* Barricades : cases bloquées (croix), aucun pion ni roi ne peut y aller ni passer dessus */
+void jeu_poser_barricade(int row, int col);
+int jeu_est_barricade(int row, int col);
 
 #endif
